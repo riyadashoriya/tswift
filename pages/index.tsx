d8d1d23d182c210ced47f72051ff6b3531b9f041
Riya Dashoriya <riyadashoriya@gmail.com>
2023-10-01 01:35:57 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,9 +21,9 @@ const Home: NextPageWithLayout = () => {
           height="315"
           src="https://www.youtube.com/embed/KudedLV0tP0?si=qY7DF2YEWdLO6H-m"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </main>
     </div>
